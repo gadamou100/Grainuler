965a060ca84b007e44d95ac548974143d351fed3
gadamou <gadamou100@hotmail.com>
2023-08-27 11:45:52 +0300

    Refactoring

--- Grainuler.DataTransferObjects/Triggers/Trigger.cs
+++ Grainuler.DataTransferObjects/Triggers/Trigger.cs
@@ -2,10 +2,11 @@
 {
     public abstract class Trigger
     {
-        public TimeSpan MaxRetryPeriod { get; set; } = TimeSpan.MaxValue;
         public TimeSpan WaitTimeWithinRetries { get; set; } = TimeSpan.FromSeconds(10);
         public bool IsExpnentialBackoffRetry { get; set; } = false;
         public ushort MaxRetryNumber { get; set; } = 3;
+        public TimeSpan MaxRetryPeriod { get; set; } = TimeSpan.MaxValue;
+
         public TimeSpan ExpireTimeSpan { get; set; } = TimeSpan.MaxValue;
         public DateTime ExpireDate { get; set; } = DateTime.MaxValue;
         public abstract string TriggerId { get; }
--- Tests/Grainuler.Tests/ScheduleTaskGrainTests.cs
+++ Tests/Grainuler.Tests/ScheduleTaskGrainTests.cs
@@ -74,78 +74,7 @@ namespace Grainuler.Tests
         }
 
 
-        [Fact]
-        public async Task TestReactiveTriggerSucess()
-        {
-            //Arrange
-            var grain = _cluster.GrainFactory.GetGrain<IScheduleTaskGrain>("test_job");
-            var reactiveGrain = _cluster.GrainFactory.GetGrain<IScheduleTaskGrain>("reactive_test_job");
-
-            var fileName = "file.txt";
-            var fileContent = "Hello world";
-            var fileInfo = new FileInfo(fileName);
-            if (fileInfo.Exists)
-                fileInfo.Delete();
-
-            var reactiveTriggerFileName = "success.txt";
-            var reactiveTriggerFileContent = "test_job has succeeded";
-            var reactiveTriggerFileInfo = new FileInfo(reactiveTriggerFileName);
-            if (reactiveTriggerFileInfo.Exists)
-                fileInfo.Delete();
-
-            var parameter = new ScheduleTaskGrainInitiationParameter
-            {
-                Payload = GetPayload(fileName,fileContent),
-                Triggers = new[]
-                {
-                    new ScheduleTrigger
-                    {
-                        RepeatTime = TimeSpan.FromMinutes(1),
-                        TriggerTime = TimeSpan.FromSeconds(5)
-                    }
-                }
-            };
-            var reactiveParameter = new ScheduleTaskGrainInitiationParameter
-            {
-                Payload = GetPayload(reactiveTriggerFileName, reactiveTriggerFileContent),
-                Triggers = new[]
-               {
-                    new TaskSuccededTrigger
-                    {
-                       TaskId ="test_job"
-                    }
-                }
-            };
-
-
-
-            //Act
-            await grain.Initiate(parameter);
-            await reactiveGrain.Initiate(reactiveParameter);
-            await Task.Delay(7000);
-
-            //Assert
-            Assert.True(File.Exists(fileInfo.FullName));
-            Assert.Equal(fileContent, File.ReadAllText(fileInfo.FullName));
-            await Task.Delay(7000);
-            Assert.True(reactiveTriggerFileInfo.Exists);
-            Assert.Equal(reactiveTriggerFileContent, File.ReadAllText(reactiveTriggerFileInfo.FullName));
-
-            //Act
-            await Task.Delay(60300);
-            string newContent = File.ReadAllText(fileInfo.FullName);
-            //Assert
-            Assert.NotEqual(fileContent, newContent);
-            Assert.Contains(fileContent, newContent);
-            await Task.Delay(3000);
-            string reactiveTriggerFileNewContent = File.ReadAllText(reactiveTriggerFileInfo.FullName);
-            Assert.NotEqual(reactiveTriggerFileContent, reactiveTriggerFileNewContent);
-
-            Assert.Contains(reactiveTriggerFileContent, reactiveTriggerFileNewContent);
-
-
-
-        }
+       
 
         private Payload GetPayload(string fileName, string fileContent, string methodName= "Execute")
         {
